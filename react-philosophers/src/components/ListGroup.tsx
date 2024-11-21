98f2f48f93e90d091df0095e24e940fd2f901e2f
MrSeemsGood <>
2024-11-21 22:47:57 +0300

    Initialize

---
+++ react-philosophers/src/components/ListGroup.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+interface PhilosopherInfo {
+    id: number;
+    name: string,
+    book: string
+}
+
+function ListGroup() {
+    let philosophers: PhilosopherInfo[] = [
+        {id: 0, name: "Платон", book: "Теэтет"},
+        {id: 1, name: "Аристотель", book: "Метафизика"},
+        {id: 2, name: "Бэкон", book: "Новый Органон"},
+    ]
+    const [phisolopherIndex, setPhisolopherIndex] = useState(-1);
+
+    return (
+        <>
+            <h1>Философы</h1>
+            <ul className="list-group">
+                {philosophers.map((p) => (
+                    <li className="list-group-item" onClick={() => setPhisolopherIndex(p.id)} key={p.name.toString()}>{p.name}</li>
+                ))}
+            </ul>
+
+            <h2>Информация о философе</h2>
+            <p>Книга: {phisolopherIndex === -1 ? "" : philosophers[phisolopherIndex].book}</p>
+        </>
+    );
+}
+
+export default ListGroup;
