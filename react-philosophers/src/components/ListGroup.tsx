d37f6df926995268048525c9c8bc7c9664d6b9a2
MrSeemsGood <>
2024-11-24 00:48:22 +0300

    Добавлена сетка

--- react-philosophers/src/components/ListGroup.tsx
+++ react-philosophers/src/components/ListGroup.tsx
@@ -2,35 +2,103 @@ import { useState } from "react";
 import styles from "./ListGroup.module.css";
 
 interface PhilosopherInfo {
-    id: number;
     name: string,
-    book: string
+    period: string,
+    book: string[]
 }
 
 function ListGroup() {
     let philosophers: PhilosopherInfo[] = [
-        {id: 0, name: "Платон", book: "Теэтет"},
-        {id: 1, name: "Аристотель", book: "Метафизика"},
-        {id: 2, name: "Бэкон", book: "Новый Органон"},
+        {name: "Платон", period: "Античность", book: ["Теэтет"]},
+        {name: "Аристотель", period: "Античность", book: ["Метафизика"]},
+        {name: "Бэкон", period: "Средние века", book: ["Новый Органон"]},
     ]
-    const [phisolopherIndex, setPhisolopherIndex] = useState(-1);
+    const [philosopherName, setPhilosopherName] = useState("");
 
     return (
         <>
             <h1>Философы</h1>
-            <ul className="list-group">
-                {philosophers.map((p) => (
-                    <li
-                        className={phisolopherIndex === p.id ? styles["list-group-item-active"] : styles["list-group-item"]}
-                        onClick={() => setPhisolopherIndex(p.id)}
-                        key={p.name.toString()}>
-                            {p.name}
-                    </li>
-                ))}
-            </ul>
 
-            <h2>Информация о философе</h2>
-            <p>Книга: {phisolopherIndex === -1 ? "" : philosophers[phisolopherIndex].book}</p>
+            <div className={styles["grid-container"]}>
+                <div className={styles["grid-item-wide-header"]}>Информация о философе</div>
+                <div className={styles["grid-item-small-header"]}>Античность</div>
+                <div className={styles["grid-item-small-header"]}>Средние века</div>
+                <div className={styles["grid-item-small-header"]}>Современность</div>
+
+                <div className={styles["grid-item-wide"]}>{
+                    philosopherName === "" 
+                    ? <p>Выберите философа, чтобы отобразить о нём информацию</p>
+                    : <>
+                        <p>Имя: {philosopherName}</p>
+                        <p>Период: {philosophers.filter((p) => p.name === philosopherName)[0].period}</p>
+                      </>
+                }</div>
+                <div className={styles["grid-item-small"]}>
+                    <ul className={styles["list-group"]}>
+                        {philosophers.filter((p) => (
+                            p.period === "Античность"
+                        )).map((p) => (
+                            <li
+                                className={philosopherName === p.name ? styles["list-group-item-active"] : styles["list-group-item"]}
+                                onClick={() => setPhilosopherName(p.name)}
+                                key={p.name.toString()}>
+                                    {p.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className={styles["grid-item-small"]}>
+                    <ul className={styles["list-group"]}>
+                        {philosophers.filter((p) => (
+                            p.period === "Средние века"
+                        )).map((p) => (
+                            <li
+                                className={philosopherName === p.name ? styles["list-group-item-active"] : styles["list-group-item"]}
+                                onClick={() => setPhilosopherName(p.name)}
+                                key={p.name.toString()}>
+                                    {p.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className={styles["grid-item-small"]}>
+                    <ul className={styles["list-group"]}>
+                        {philosophers.filter((p) => (
+                            p.period === "Современность"
+                        )).map((p) => (
+                            <li
+                                className={philosopherName === p.name ? styles["list-group-item-active"] : styles["list-group-item"]}
+                                onClick={() => setPhilosopherName(p.name)}
+                                key={p.name.toString()}>
+                                    {p.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className={styles["grid-item-small-header"]}>Термины</div>
+                <div className={styles["grid-item-small-header"]}>Направления</div>
+                <div className={styles["grid-item-small-header"]}>Книги</div>
+                <div className={styles["grid-item-wide"]}></div>
+
+                <div className={styles["grid-item-small"]}></div>
+                <div className={styles["grid-item-small"]}></div>
+                <div className={styles["grid-item-small"]}>
+                    <ul className={styles["list-group"]}>
+                        {philosophers.filter((p) => (
+                            p.name === philosopherName
+                        )).map((p) => (
+                            <li
+                                className={styles["list-group-item"]}
+                                key={p.book.toString()}>
+                                    {p.book}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </>
     );
 }
